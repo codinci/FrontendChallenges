68b59a934e1d9ce85cfbe1586804d91f4ba47f7c
codinci <dkibau@gmail.com>
2023-12-06 09:53:15 +0300

    Added slider functionality for images

--- room-homepage-master/js/slider.js
+++ room-homepage-master/js/slider.js
@@ -8,15 +8,21 @@ export default class Slider {
 		this.slides = [
 			{
 				title: `Discover innovative ways to decorate`,
-				description: `We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.`
+				description: `We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.`,
+				srcset: `../images/mobile-image-hero-1.jpg`,
+				src: `../images/desktop-image-hero-1.jpg`
 			},
 			{
 				title: `We are available all across the globe`,
-				description: `With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.`
+				description: `With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.`,
+				srcset: `../images/mobile-image-hero-2.jpg`,
+				src: `../images/desktop-image-hero-2.jpg`
 			},
 			{
 				title: `Manufactured with the best materials`,
-				description: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.`
+				description: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.`,
+				srcset: `../images/mobile-image-hero-3.jpg`,
+				src: `../images/desktop-image-hero-3.jpg`
 			}
 		]
 
@@ -67,8 +73,9 @@ export default class Slider {
 	}
 
 	showSlide(slideNumber) {
-		console.log(slideNumber)
 		this.slideHeader.textContent = this.slides[slideNumber].title
 		this.slideText.textContent = this.slides[slideNumber].description
+		this.heroImage.querySelector('source').srcset = this.slides[slideNumber].srcset
+		this.heroImage.querySelector('img').src = this.slides[slideNumber].src
 	}
 }
